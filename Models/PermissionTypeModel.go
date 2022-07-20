5002ab064eb4e5e56bdc1566d7dbb23d21957663
Hariprasad Pujari <hariprasadp.iitkgp@gmail.com>
2022-07-20 11:49:46 +0530

    Added Models (vineela)

---
+++ Models/PermissionTypeModel.go
@@ -0,0 +1,15 @@
+package Models
+
+type PermissionType struct {
+	Id      int    `json:"_id"`
+	Name    string `json:"name"`
+	Add     bool   `json:"add"`
+	Delete  bool   `json:"delete"`
+	Update  bool   `json:"update"`
+	Enable  bool   `json:"enable"`
+	Disable bool   `json:"disable"`
+}
+
+func (pt *PermissionType) TableName() string {
+	return "permission-type"
+}
