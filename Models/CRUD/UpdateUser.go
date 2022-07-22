b5194fa99b5be3cc4b68563efe0068dfc128e748
Hariprasad Pujari <hariprasadp.iitkgp@gmail.com>
2022-07-22 15:40:22 +0530

    Verified all api end points

--- Models/CRUD/UpdateUser.go
+++ Models/CRUD/UpdateUser.go
@@ -3,6 +3,7 @@ package CRUD
 import (
 	"cronService/Database"
 	"cronService/Models"
+	"time"
 )
 
 func UpdateUser(user Models.User)(err error){
@@ -11,9 +12,10 @@ func UpdateUser(user Models.User)(err error){
 	err=Database.DB.Find(&user_new,"id=?",user.Id).Error;if err!=nil{
 		return err
 	}
-	Database.DB.Model(user_new).Update(
-		"PermissionID",
-		user.PermissionID,
-	)
+	user_new.PermissionID = user.PermissionID
+	user_new.UpdatedAT = time.Now().Unix()
+	user.UpdatedAT = time.Now().Unix()
+	Database.DB.Save(user_new)
+	//Database.DB.Model(user_new).Update(Models.User{PermissionID: user.PermissionID, UpdatedAT: time.Now().Unix()})
 	return nil
 }
